6fda9a3076c1ff761acf87866b5bf83a0ed8d806
thmontgomery <tmontgomery@bama.ua.edu>
2019-09-13 15:29:00 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Taylor Montgomery on 9/13/19.
+//  Copyright © 2019 Taylor Montgomery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
